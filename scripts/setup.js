75ab154e4c12a9727dc9f43160742e4d082cfa6a
Gary Hai <gary@xl59.com>
2015-04-17 16:32:16 +0800

    Initialize graph in setup.js.

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,14 +3,19 @@
 
   var console = require("console");
   var db = require("org/arangodb").db;
-  var createCollection = function(collection) {
-    var name = applicationContext.collectionName(collection);
-    if (db._collection(name) === null) {
-      db._create(name);
-    } else if (applicationContext.isProduction) {
-      console.warn("collection '%s' already exists. Leaving it untouched.", name);
-    }
+  var gm = require("org/arangodb/general-graph");
+  var createGraph = function(graph) {
+    var g_name = applicationContext.graphName(graph),
+        v_name = applicationContext.vertexCollection(graph),
+        e_name = applicationContext.edgeCollection(graph);
+    var existence = gm._exists(g_name) || db._exists(v_name) || db._exists(e_name);
+    if (existence && applicationContext.isProduction) {
+      console.warn("collections of graph '%s' already exists. Leaving it untouched.", g_name);
+    } else {
+      gm._create(g_name, [gm._relation(e_name, v_name, v_name)], []);
+    };
   };
 
+  createGraph("de");
   
 }());
