3aabff016575498a9d00cb22abe994ccb86817ea
Gary Hai <gary@xl59.com>
2015-04-17 16:48:31 +0800

    Misunderstand applicationContext.

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,9 +5,11 @@
   var db = require("org/arangodb").db;
   var gm = require("org/arangodb/general-graph");
   var createGraph = function(graph) {
-    var g_name = applicationContext.graphName(graph),
+/*    var g_name = applicationContext.graphName(graph),
         v_name = applicationContext.vertexCollection(graph),
         e_name = applicationContext.edgeCollection(graph);
+*/
+    var g_name = "de_graph", v_name = "de_node", e_name = "de_link";
     var existence = gm._exists(g_name) || db._exists(v_name) || db._exists(e_name);
     if (existence && applicationContext.isProduction) {
       console.warn("collections of graph '%s' already exists. Leaving it untouched.", g_name);
